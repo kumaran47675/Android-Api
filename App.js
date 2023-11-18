03fa5346c215dfc1773859a312c334e80c7539fe
kumaran47675 <133028937+kumaran47675@users.noreply.github.com>
2023-11-18 21:44:06 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNav from './TabNav';
-import DeleteMethod from './pages/DeleteScreen';
+import GetScreen from './pages/GetScreen.js';
+import DeleteScreen from './pages/DeleteScreen.js';
+
+import PostScreen from './pages/PostScreen.js';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import 'react-native-gesture-handler';
 const Drawer = createDrawerNavigator();
@@ -10,8 +13,10 @@ const App = () => {
   return (
     <NavigationContainer>
     <Drawer.Navigator>
-    <Drawer.Screen name="TabNav" component={TabNav} />
-    <Drawer.Screen name="Delete" component={DeleteMethod} />
+    <Drawer.Screen name="Put" component={TabNav} />
+    <Drawer.Screen name="Delete" component={DeleteScreen} />
+    <Drawer.Screen name="Post" component={PostScreen} />
+    <Drawer.Screen name="Get" component={GetScreen} />
   </Drawer.Navigator>
   </NavigationContainer>
    );
